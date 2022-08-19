bf4bb9779dfb99195ef13af055ad70edd6f9fd63
Bahaulddin Shammary <bahaa.shammary@gmail.com>
2022-08-20 00:17:51 +0100

    Finished chapter 4

--- Typescript Project/src/ch4/ch4_v2.ts
+++ Typescript Project/src/ch4/ch4_v2.ts
@@ -3,7 +3,10 @@
 
 // Remaking the Query to use it as a generic type to specify
 // the type of parameter we pass to matches function
-
+type GenericQuery<TProp> = {
+    sort?: 'asc' | 'desc';
+    matches(val: TProp): boolean;
+}
 // Use mapped type definition
 // Using the previous example of ContactQuery
 // Instead of using this way: 
@@ -13,5 +16,5 @@
 // It is like a loop that maps the properties of Contact to this new type
 // The question mark makes all properties optional
 type MappedContactQuery = {
-    [TProp in keyof Contact]?: GenericQuery
+    [TProp in keyof Contact]?: GenericQuery<Contact[TProp]>
 }
